fec458f3eae14f12c33ae4902fe1a14ff70c0474
Manu3l02 <manuel.altavela@gmail.com>
2025-09-01 21:40:09 +0200

    init: chatto backend project

---
+++ backend/src/main/java/io/manuel/chatto/ChattoApplication.java
@@ -0,0 +1,13 @@
+package io.manuel.chatto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/manuel/chatto/ChattoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.manuel.chatto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
